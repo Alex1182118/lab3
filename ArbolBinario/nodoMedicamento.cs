7b1e0e43ad1f1b4ee06da515b251714e3e9f3c2c
Alex1182118 <alex.gabrielvillatoro@gmail.com>
2019-03-07 18:19:05 -0600

    Alexander Villatoro 1182118
    Arboles y Archivos

---
+++ ArbolBinario/nodoMedicamento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolBinario
+{
+    class nodoMedicamento
+
+    {
+        public int IdNodo { get; set; }
+        public string NameNodo { get; set; }
+        public int LineNodo { get; set; }
+        public nodoMedicamento()
+        {
+        }
+    }
+}
